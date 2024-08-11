dbd50012e128a1cee985cf1a3676b3acb340fe91
ColmofWar <joshuaallendimarco@gmail.com>
2024-08-10 23:04:25 -0400

    finish count-zeroes.js and find-floor.js

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,18 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    target = 0
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+    if (arr[leftIdx] === target){
+        return arr.length;
+    }
+    while(arr[rightIdx] == target){
+        let middleIdx = Math.floor((leftIdx + rightIdx)/2);
+        let middleVal = arr[middleIdx];
+        if(middleVal != target ){
+            leftIdx = middleIdx + 1;
+        } else if (middleVal === target){
+            rightIdx = middleIdx - 1;
+        }
+    }
+    return (arr.length - (rightIdx + 1));
 }
-
-module.exports = countZeroes
--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,24 @@
-function findFloor() {
-  
+function findFloor(arr,target) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+    if (arr[leftIdx] > target){
+        return -1;
+    }
+    while(leftIdx <= rightIdx){
+        let middleIdx = Math.floor((leftIdx + rightIdx)/2);
+        let middleVal = arr[middleIdx];
+        if(middleVal < target ){
+            leftIdx = middleIdx + 1;
+        } else if (middleVal > target){
+            rightIdx = middleIdx - 1;
+        } else {
+            return middleIdx;
+        }
+    }
+    while(arr[rightIdx] > target){
+        rightIdx --;
+    }
+    return arr[rightIdx]
 }
 
-module.exports = findFloor
+//module.exports = findFloor
